2d2845bc7bb69314b840cdee6cd74d0cf282319c
Giulia Stazio <giuliastazio@gmail.com>
2025-12-09 00:29:11 +0100

    Abbozzata un prima schermata per il login

---
+++ bugboard26-frontend/src/main/java/it/ludina/bugboard26fe/bugboard26frontend/HelloController.java
@@ -0,0 +1,7 @@
+package it.ludina.bugboard26fe.bugboard26frontend;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+}
---
+++ bugboard26-frontend/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module it.ludina.bugboard26fe.bugboard26frontend {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens it.ludina.bugboard26fe.bugboard26frontend to javafx.fxml;
+    exports it.ludina.bugboard26fe.bugboard26frontend;
+}
